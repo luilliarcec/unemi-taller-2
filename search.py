c136c581889203d08dce0612d13d99590ecf215d
netocondor10 <netobsc@gmail.com>
2025-10-05 08:51:33 -0500

    Create search.py

---
+++ search.py
@@ -0,0 +1,12 @@
+import csv
+
+CSV_FILE = 'contacts.csv'
+
+def read_contacts():
+    with open(CSV_FILE, mode='r') as file:
+        reader = csv.DictReader(file)
+        return list(reader)
+
+def search_contact(query):
+    contacts = read_contacts()
+    return [c for c in contacts if c['Name'] == query or c['Phone'] == query]
